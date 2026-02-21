6dfc6c8f8bcd1c2cef895332cf620eaee60a68b0
Prat06-creator <nandipratyusha6@gmail.com>
2026-02-22 01:38:29 +0530

    changed index.js

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,10 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
+  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
   credentials: true
 }));
+app.options("*", cors(corsOptions));
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({limit:"50mb",extended:true}));
 app.use(cookieParser())
