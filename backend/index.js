1d964e1c97caeada4d064c30973110f06f4c88cc
Prat06-creator <nandipratyusha6@gmail.com>
2026-02-22 08:50:48 +0530

    plssss runnn

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({limit:"50mb",extended:true}));
 app.use(cookieParser())
